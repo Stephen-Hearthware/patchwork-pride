11e012055f5f8e6c29163220a7d4024aa290f035
Stephen-Hearthware <stephen@hearthware.org>
2025-06-22 12:53:16 -0500

    feat: add basic App component (#7)

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <header className="bg-gradient-to-r from-pink-500 to-purple-600 py-4 text-white">
+        <h1 className="text-center text-2xl font-bold">Patchwork Pride</h1>
+      </header>
+      <main className="p-4">
+        <p className="text-gray-700">Content coming soon.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
